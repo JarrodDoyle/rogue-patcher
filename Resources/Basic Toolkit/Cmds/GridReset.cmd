6df9ffe13443397476f350fb06cf4da2f447675d
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-08-30 12:10:49 +0100

    Add vfig nudge grid scripts to basic toolkit

---
+++ Resources/Basic Toolkit/Cmds/GridReset.cmd
@@ -0,0 +1,29 @@
+ 
+;; Reset both grids to 13 (0.5 unit scale)
+
+; reset nudge scale (min 1, max 1024) to default of 4
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 1
+global_scale 1
+; zoom back in to compensate (default camera scale is 64, but cant change this?)
+;zoom_all 0.50
+; reset to grid scale 13
+grid_abs_scale 0.50
+
+;; And increase to 14 (1 unit scale)
+
+; zoom out / increase nudge scale
+global_scale 1
+; zoom back in to compensate
+zoom_all 0.50
+; increase grid scale
+grid_scale 2.00
